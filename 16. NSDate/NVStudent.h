6eff058954024c5e630c5f0a612d642b60d1d8f9
NSemakov <nsemakov@yahoo.com>
2015-07-26 22:48:39 +0400

    All levels

---
+++ 16. NSDate/NVStudent.h
@@ -0,0 +1,16 @@
+//
+//  NVStudent.h
+//  16. NSDate
+//
+//  Created by Admin on 26.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NVStudent : NSObject
+@property (strong,nonatomic) NSDate *dateOfBirth;
+@property (strong,nonatomic) NSString* firstname;
+@property (strong,nonatomic) NSString* lastname;
+
+@end
